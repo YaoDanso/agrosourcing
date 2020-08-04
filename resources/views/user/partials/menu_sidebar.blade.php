5e41424d50b7a6d717c7f6c59051bce7e8585bda
Andrews Quarcoo <andy@Andrews-MacBook-Pro.local>
2020-08-04 13:34:56 +0000

    lol

--- resources/views/user/partials/menu_sidebar.blade.php
+++ resources/views/user/partials/menu_sidebar.blade.php
@@ -49,7 +49,7 @@
             <li class="nav-item">
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseFunding" aria-expanded="true" aria-controls="collapsePages">
                     <i class="fas fa-fw fa-box-open"></i>
-                    <span>Product</span>
+                    <span>Processing Company</span>
                 </a>
                 <div id="collapseFunding" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
