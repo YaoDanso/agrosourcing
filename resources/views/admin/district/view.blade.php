9dd294fda4008b5413cf41f47f31a7feb2606814
Andrews Quarcoo <yawsonandrews@gmail.com>
2020-11-01 13:43:22 +0000

    changed district controller

--- resources/views/admin/district/view.blade.php
+++ resources/views/admin/district/view.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800">User Roles</h1>
+        <h1 class="h3 mb-0 text-gray-800">Available Districts</h1>
     </div>
     <div class="row">
         <div class="col-lg-7 col-md-7 col-sm-12 col-xs-12">
